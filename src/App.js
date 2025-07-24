4d65734e1488781c5e911e80c00ba72a26a7409f
Kamlesh Savant <sawantk45@gmail.com>
2025-07-24 15:41:35 +0530

    completed project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div>Hello</div>;
+  const [data, setData] = useState([]);
+
+  const fecthImages = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+    // console.log(response.data);
+  };
+
+  useEffect(() => {
+    fecthImages();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Gallery</h1>
+      </header>
+      <div className="gallery">
+        {data.map((image) => {
+          return <img src={image.download_url} height={400} alt="" />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
